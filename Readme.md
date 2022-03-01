ec50bc33305da7518569560e46ad2a876f6793f8
Jordan Hanley <jhanley@twilio.com>
2022-03-01 15:42:41 +0000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Delete call recordings for a phone number
+This application is an implementation to show how you could use Twilio's helper libraries to quickly discover calls for a given filter criteria and then retrieve and optionally delete the recordings for them calls
+
+## Setup
+Firstly retreive your Account SID and Auth Code from the twilio console and set the `TWILIO_ACCOUNT_SID` and `TWILIO_AUTH_TOKEN` environement variables, for details see how to [Store Your Twilio Credentials Securely](https://www.twilio.com/docs/usage/secure-credentials).
+
+## Operation
+This application has 2 variables that you can set
+1. `fromNumber` this is the number that you are expecting the calls you are querying to be from using E.164 format (+44XXXXXXXXXX).
+2. `shouldActuallyDelete` use this variable if you would like to do a dry run, in this instance recordings will not be deleted.
+
+This code has been written for proof of concept purposes. Please fully test this code before running in your production environment
---
+++ RecordingsDelete/Program.cs
@@ -0,0 +1,57 @@
+﻿// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        //The flters for fetching of the call resources
+        string fromNumber = "";
+
+        //set this to false for a dry run
+        bool shouldActuallyDelete = false;
+
+        //Here we fetch all calls for a given filter criteria, in this case from number and status 'completed'
+        var calls = await CallResource.ReadAsync(
+            status: CallResource.StatusEnum.Completed,
+            from: new Twilio.Types.PhoneNumber(fromNumber)
+        );
+
+        //loop through calls
+        foreach (var call in calls)
+        {
+            Console.WriteLine($"call sid is {call.Sid}");
+
+            //fetch recordings for a given call and loop them
+            var recordings = await RecordingResource.ReadAsync(callSid: call.Sid);
+
+            foreach(var recording in recordings)
+            {
+                // if we should delete the call then delete it, else just output
+                if (shouldActuallyDelete)
+                {
+                    Console.WriteLine($"Deleting {recording.Sid}");
+                    RecordingResource.Delete(pathSid: recording.Sid);
+                }
+                else
+                {
+                    Console.WriteLine($"Would have deleted {recording.Sid}");
+                }
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
